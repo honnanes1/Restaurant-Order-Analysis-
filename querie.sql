7b821cf32338833b5b4ce93dd8bd2c7360807ed4
Mohammed Hannane <71062477+honnanes1@users.noreply.github.com>
2024-03-12 21:58:22 +0000

    Update querie.sql

--- querie.sql
+++ querie.sql
@@ -1,6 +1,6 @@
--- Task 1  View the menu_items table and write a query to find the number of items on the menu
+--  View the menu_items table and write a query to find the number of items on the menu
     SELECT count(*) AS menu_items FROM restaurant_db.menu_items;
--- Task 2 What are the least and most expensive items on the menu?
+-- What are the least and most expensive items on the menu?
 -- Most expensive item
 SELECT
     item_name as most_expensive_item,
@@ -19,7 +19,7 @@ FROM
 ORDER BY
     price asc
 limit 1; 
--- Task 3  How many Italian dishes are on the menu? What are the least and most expensive Italian dishes on the menu?
+-- How many Italian dishes are on the menu? What are the least and most expensive Italian dishes on the menu?
 -- How many Italian dishes are on the menu?
 select
     COUNT(item_name) AS italian_dish_count
@@ -51,7 +51,7 @@ ORDER BY
     price DESC
 LIMIT 1;
 
--- Task 4 How many dishes are in each category? What is the average dish price within each category?
+-- How many dishes are in each category? What is the average dish price within each category?
 select
     category,
 	count(item_name) as num_dishes, 
@@ -60,20 +60,20 @@ FROM
     menu_items
 GROUP BY
     category;
--- Task 5 View the order_details table. What is the date range of the table?
+-- View the order_details table. What is the date range of the table?
 
--- Task 6  How many orders were made within this date range? How many items were ordered within this date range?
+--  How many orders were made within this date range? How many items were ordered within this date range?
 
--- Task 7  Which orders had the most number of items?
+--  Which orders had the most number of items?
 
--- Task 8  How many orders had more than 12 items?
+--  How many orders had more than 12 items?
 
- -- Task 9  Combine the menu_items and order_details tables into a single table
+ --  Combine the menu_items and order_details tables into a single table
 
 
- -- Task 10  What were the least and most ordered items? What categories were they in?
+ --  What were the least and most ordered items? What categories were they in?
 
- -- Task 11 What were the top 5 orders that spent the most money?
+ --  What were the top 5 orders that spent the most money?
 
- -- Task 12 View the details of the highest spend order. Which specific items were purchased?
+ -- View the details of the highest spend order. Which specific items were purchased?
 

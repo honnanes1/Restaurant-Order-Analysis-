8497a69c3c5ecc72d5a3dc5358a8a9a20bdfd2f7
honnanes1 <honnanes1@gmail.com>
2024-03-12 13:41:14 +0000

    query

---
+++ querie.sql
@@ -0,0 +1,26 @@
+-- Task 1  View the menu_items table and write a query to find the number of items on the menu
+
+-- Task 2 What are the least and most expensive items on the menu?
+
+-- Task 3  How many Italian dishes are on the menu? What are the least and most expensive Italian dishes on the menu?
+
+
+-- Task 4 How many dishes are in each category? What is the average dish price within each category?
+
+-- Task 5 View the order_details table. What is the date range of the table?
+
+-- Task 6  How many orders were made within this date range? How many items were ordered within this date range?
+
+-- Task 7  Which orders had the most number of items?
+
+-- Task 8  How many orders had more than 12 items?
+
+ -- Task 9  Combine the menu_items and order_details tables into a single table
+
+
+ -- Task 10  What were the least and most ordered items? What categories were they in?
+
+ -- Task 11 What were the top 5 orders that spent the most money?
+
+ -- Task 12 View the details of the highest spend order. Which specific items were purchased?
+
